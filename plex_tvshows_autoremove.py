467a1d401dca214ad2d5add365390cc7fbd1c088
Alfonso C. Betancort <abetancort@users.noreply.github.com>
2016-02-18 23:57:08 +0000

    Update plex_tvshows_autoremove.py

--- plex_tvshows_autoremove.py
+++ plex_tvshows_autoremove.py
@@ -14,7 +14,7 @@ print 'Running Plex Cleaner on '+datetime.datetime.now().strftime('%m/%d/%Y %H:%
 print ''
 for entry in plex.library.section('TV Shows').recentlyViewed():
     tvFile = entry.media[0].parts[0].file
-    if entry.grandparentTitle not in skipTV:
+    if entry.grandparentTitle not in skipTV and :
         if entry not in plex.library.onDeck():
             print 'Deleting '+entry.title+' ::: '+tvFile
             deleted += 1

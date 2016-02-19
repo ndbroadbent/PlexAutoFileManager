b8d82ea57da9629ae913b6b5d2a886adcc711eb3
Alfonso C. Betancort <abetancort@users.noreply.github.com>
2016-02-19 09:36:37 +0000

    Update plex_tvshows_autoremove.py

--- plex_tvshows_autoremove.py
+++ plex_tvshows_autoremove.py
@@ -15,14 +15,14 @@ print ''
 for entry in plex.library.section('TV Shows').recentlyViewed():
     tvFile = entry.media[0].parts[0].file
     if entry.grandparentTitle not in skipTV and entry not in plex.library.onDeck():
-            print 'Deleting '+entry.title+' ::: '+tvFile
-            deleted += 1
-            os.remove(tvFile)
-            for sim in simFiles:
-                simFile = os.path.splitext(tvFile)[0]+sim
-                if os.path.isfile(simFile):
-                    print '::: Deleting it\'s similar file too ::: '+simFile
-                    os.remove(simFile)
+        print 'Deleting '+entry.title+' ::: '+tvFile
+        deleted += 1
+        os.remove(tvFile)
+        for sim in simFiles:
+            simFile = os.path.splitext(tvFile)[0]+sim
+            if os.path.isfile(simFile):
+                print '::: Deleting it\'s similar file too ::: '+simFile
+                os.remove(simFile)
     else:
         kept += 1
 print ''

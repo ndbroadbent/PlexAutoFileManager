027282110046cf4e655617394cd38fa7ceab0d8c
Alfonso C. Betancort <abetancort@users.noreply.github.com>
2016-02-19 10:49:21 +0000

    Update plex_tvshows_autoremove.py

--- plex_tvshows_autoremove.py
+++ plex_tvshows_autoremove.py
@@ -4,7 +4,7 @@ import os, datetime
 from plexapi.server import PlexServer
 plex = PlexServer()
 # Todo: move to its own separate setting.cfg file
-doTV=1 # defaul action. Use 1 for deleting or 2 for keeping TV Shows
+doTV=1 # default action. Use 1 for deleting or 0 for keeping TV Shows
 skipTV = []
 simFiles = ['.srt', '.nfo', '.tbn', '.nzb', '.nfo-orig', '.sfv', '.srr', '.jpg', '.png', '.jpeg', '.txt', '.idx', '.sub']
 kept=0
@@ -14,7 +14,9 @@ print 'Running Plex Cleaner on '+datetime.datetime.now().strftime('%m/%d/%Y %H:%
 print ''
 for entry in plex.library.section('TV Shows').recentlyViewed():
     tvFile = entry.media[0].parts[0].file
-    if entry.grandparentTitle not in skipTV and entry not in plex.library.onDeck():
+    """ (bool(doTV) is not bool(entry.grandparentTitle not in skipTV)) is the equivalent to a logic xor in Python
+        as far as I know there is no logic operand to do that directly """ 
+    if (bool(doTV) is not bool(entry.grandparentTitle not in skipTV)) and entry not in plex.library.onDeck():
         print 'Deleting '+entry.title+' ::: '+tvFile
         deleted += 1
         os.remove(tvFile)

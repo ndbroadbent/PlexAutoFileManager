a648a3cb6d71f9c90fa736d772f5ff6865d44e16
Alfonso C. Betancort <abetancort@users.noreply.github.com>
2016-02-19 11:15:20 +0000

    Update plex_tvshows_autoremove.py

--- plex_tvshows_autoremove.py
+++ plex_tvshows_autoremove.py
@@ -3,8 +3,10 @@
 import os, datetime
 from plexapi.server import PlexServer
 plex = PlexServer()
-# Todo: move to its own separate setting.cfg file
-doTV=1 # default action. Use 1 for deleting or 0 for keeping TV Shows
+""" Todo: Change to directive """
+trialRun = 1 # Testing purposes. Use 1 for normal use. Set to 0 for debugging/testing runs that don't delete anything.
+""" Todo: move to its own separate setting.cfg file """
+doTV=1 # default action. Use 1 for deleting; 0 for keeping TV Shows, all but those in skipTV[]
 skipTV = []
 simFiles = ['.srt', '.nfo', '.tbn', '.nzb', '.nfo-orig', '.sfv', '.srr', '.jpg', '.png', '.jpeg', '.txt', '.idx', '.sub']
 kept=0
@@ -14,17 +16,17 @@ print 'Running Plex Cleaner on '+datetime.datetime.now().strftime('%m/%d/%Y %H:%
 print ''
 for entry in plex.library.section('TV Shows').recentlyViewed():
     tvFile = entry.media[0].parts[0].file
-    """ (bool(doTV) is not bool(entry.grandparentTitle not in skipTV)) is the equivalent to a logic xor in Python
-        as far as I know there is no logic operand to do that directly """ 
-    if (bool(doTV) is not bool(entry.grandparentTitle not in skipTV)) and entry not in plex.library.onDeck():
+    """ bool(doTV) is not bool(entry.grandparentTitle in skipTV) is a logic XOR, in Python
+        as far as I know there is no logic operand to do XOR (only bitwise xor) """ 
+    if (bool(doTV) is not bool(entry.grandparentTitle in skipTV)) and entry not in plex.library.onDeck():
         print 'Deleting '+entry.title+' ::: '+tvFile
         deleted += 1
-        os.remove(tvFile)
+        if trialRun: os.remove(tvFile)
         for sim in simFiles:
             simFile = os.path.splitext(tvFile)[0]+sim
             if os.path.isfile(simFile):
                 print '::: Deleting it\'s similar file too ::: '+simFile
-                os.remove(simFile)
+                if trialRun: os.remove(simFile)
     else:
         kept += 1
 print ''
